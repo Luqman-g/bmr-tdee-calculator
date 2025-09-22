01b1fb8c8c2ed25fc08b2428060fda5b9b802d2b
Luqman Gassama <luqmangassama@yahoo.com>
2025-09-22 17:22:02 -0400

    update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Welcome to my site</h1>
+      <h2 className="text-3xl font-bold">Luqman Design</h2>
     </main>
   );
 }
