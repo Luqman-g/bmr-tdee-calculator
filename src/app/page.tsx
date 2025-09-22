5b7de5f57dbfa6cbe5cf5b6bd21ecca4330c1f28
Luqman Gassama <luqmangassama@yahoo.com>
2025-09-22 17:28:04 -0400

    update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import BmrTdeeCalculator from "@/components/BmrTdeeCalculator";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Welcome to my site</h1>
-      <h2 className="text-3xl font-bold">Luqman Design</h2>
+    <main className="flex min-h-screen items-center justify-center px-4">
+      <BmrTdeeCalculator />
     </main>
   );
 }
